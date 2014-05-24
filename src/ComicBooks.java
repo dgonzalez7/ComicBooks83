1942a4b1c88aab63b9c1a6530df9ea0119afb4b3
Dave Gonzalez <dgonzalez@thosegonzos.com>
2014-05-24 18:32:08 -0500

    Success with Generics.

--- src/ComicBooks.java
+++ src/ComicBooks.java
@@ -7,7 +7,7 @@ public class ComicBooks {
 	public static void main(String[] args) 
 	{
 		// Set up hash map
-		HashMap quality = new HashMap();
+		HashMap<String, Float> quality = new HashMap<>();
 		float price1 = 3.00F;
 		quality.put("mint",price1);
 		float price2 = 2.00F;
@@ -24,11 +24,11 @@ public class ComicBooks {
 		// Set up collection
 		Comic[] comix = new Comic[3];
 		comix[0] = new Comic("Amazing Spider-Man", "1A", "very fine", 5_000.00F);
-		comix[0].setPrice( (Float) quality.get(comix[0].condition));
+		comix[0].setPrice(quality.get(comix[0].condition));
 		comix[1] = new Comic("Incredible Hulk", "181", "near mint", 240.00F);
-		comix[1].setPrice( (Float) quality.get(comix[1].condition));
+		comix[1].setPrice(quality.get(comix[1].condition));
 		comix[2] = new Comic("Cerebus", "1A", "good", 144.00F);
-		comix[2].setPrice( (Float) quality.get(comix[2].condition));
+		comix[2].setPrice(quality.get(comix[2].condition));
 		
 		for (int i = 0; i < comix.length; i++)
 		{
